6739fef7cd7f11afc31e5b404d342d1e5bc505c7
serj-lubin <s.yu.lubin@gmail.com>
2023-05-25 15:49:10 +0300

    Update main.go

--- main.go
+++ main.go
@@ -9,21 +9,24 @@ import (
 )
 
 func main() {
-// 	const ShellToUse = "sh"
-// 	cmd := exec.Command(ShellToUse, "-c", "  echo 1 >/tmp/TeSt")
-// 	err := cmd.Run()
+	// 	const ShellToUse = "sh"
+	// 	cmd := exec.Command(ShellToUse, "-c", "  echo 1 >/tmp/TeSt")
+	// 	err := cmd.Run()
 	files, err := ioutil.ReadDir(".")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, file := range files {
-		fmt.Println(file.Name(), file.IsDir())
-		Original_Path :=  file.Name()
+
+		Original_Path := file.Name()
 		New_Path := strings.ToLower(Original_Path)
-		e := os.Rename(Original_Path, New_Path)
-		if e != nil {
-			log.Fatal(e)
+		if Original_Path != New_Path {
+			fmt.Println(file.Name(), file.IsDir())
+			e := os.Rename(Original_Path, New_Path)
+			if e != nil {
+				log.Fatal(e)
+			}
 		}
 	}
 
